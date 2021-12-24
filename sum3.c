0b3b13680de43115952a84e036b5f3028c3dd644
Cloud User <ec2-user@ip-172-31-2-20.us-east-2.compute.internal>
2021-12-24 13:24:07 +0000

    file added

--- sum3.c
+++ sum3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-sum3{    
-
-    int number1, number2, sum;
-    
-    printf("Enter two integers: ");
-    scanf("%d %d", &number1, &number2);
-
-    // calculating sum
-    sum = number1 + number2;      
-    
-    printf("%d + %d = %d", number1, number2, sum);
- //   return 0;
+sum3()
+{
+      int n,l,f,m,sum,t1;
+      printf("Enter 3-Digit Number: ");
+      scanf("%d",&n);
+      f=n/100;
+      t1=n%100;
+      l=t1%10;
+      m=t1/10;
+      printf("\nFirst Digit  = %d \nMiddle Digit = %d \nLast Digit   = %d\n",f,m,l);
+      sum=l+m+f;
+      printf("\nSum of All 3-Digits : %d",sum);
+ //     return 0;
 }
-

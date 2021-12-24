0fea7004296d39c8663b9a45c23deb186bcb6f26
Cloud User <ec2-user@ip-172-31-2-20.us-east-2.compute.internal>
2021-12-24 11:51:56 +0000

    rev.c is updated

--- rev.c
+++ rev.c
@@ -1,6 +1,6 @@
 #include <stdio.h>  
 #include <string.h>  
-int main()  
+rev()  
 {  
     char str[40]; // declare the size of character string  
     printf (" \n Enter a string to be reversed: ");  
@@ -8,5 +8,5 @@ int main()
       
     // use strrev() function to reverse a string  
     printf (" \n After the reverse of a string: %s ", strrev(str));  
-    return 0;  
+   // return 0;  
 }  
